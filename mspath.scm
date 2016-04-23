64ae6b7927dee4871023936d5f02dbf38a03d75d
Hamayama <hamay1010@gmail.com>
2016-04-23 14:42:47 +0900

    src chg v1.02

--- mspath.scm
+++ mspath.scm
@@ -1,16 +1,16 @@
 ;; -*- coding: utf-8 -*-
 ;;
 ;; mspath.scm
-;; 2016-4-21 v1.01
+;; 2016-4-23 v1.02
 ;;
 ;; ＜内容＞
-;;   Gauche の REPL 上で、Windows のパス名を文字列に変換するモジュールです。
-;;   ただし、パス名は、ダブルクォートではなくて '() で囲って渡す必要があります。
+;;   Gauche の REPL 上で、Windows のパス名をそのまま読み込むためのモジュールです。
 ;;
 ;;   詳細については、以下のページを参照ください。
 ;;   https://github.com/Hamayama/mspath
 ;;
 (define-module mspath
+  (use gauche.version)
   (use srfi-13) ; string-trim-both用
   (export
     mspath msload msrun))
@@ -20,7 +20,8 @@
 ;;   ・パス名は、ダブルクォートではなくて '() で囲って渡す必要がある。
 ;;     例えば、以下のように変換される。
 ;;       '(c:\work\aaa.txt) → "c:\\work\\aaa.txt"
-(define (mspath path-data)
+;;   ・パス名を省略すると、入力待ちになる。このときは '() は入力不要。
+(define (mspath :optional (path-data #f))
   (let1 path-str
       (cond
        ((list? path-data)
@@ -34,20 +35,26 @@
                     (string-append " " str))))
               path-data)
          ""))
+       ((not path-data)
+        (display "file : ") (flush)
+        (if (version<=? (gauche-version) "0.9.4") (read-line))
+        (regexp-replace-all #/"/ (read-line) ""))
        (else
-        (errorf "list required, but got ~s" path-data)))
+        (errorf "list or #f required, but got ~s" path-data)))
     (set! path-str (string-trim-both path-str))
     ;(write path-str) (newline) (flush)
     path-str))
 
 ;; Windows のパス名でロードを行う
 ;;   ・パス名は、ダブルクォートではなくて '() で囲って渡す必要がある。
-(define (msload path-data)
+;;   ・パス名を省略すると、入力待ちになる。このときは '() は入力不要。
+(define (msload :optional (path-data #f))
   (load (mspath path-data)))
 
-;; Windows のパス名でロードを行い、main 手続きがあれば実行する
+;; Windows のパス名でロードを行い、main 手続きを実行する
 ;;   ・パス名は、ダブルクォートではなくて '() で囲って渡す必要がある。
-(define (msrun path-data :optional (args '()))
+;;   ・パス名を省略すると、入力待ちになる。このときは '() は入力不要。
+(define (msrun :optional (path-data #f) (args '()))
   (load (mspath path-data))
   (if (global-variable-bound? 'user 'main)
     (with-module user (if (procedure? main)
